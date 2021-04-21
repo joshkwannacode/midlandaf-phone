e67488b8f0128d361a7af4eb9082c618b2f3d003
joshkwannacode <65772281+joshkwannacode@users.noreply.github.com>
2021-04-21 15:25:39 +0200

    Add files via upload

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -14,23 +14,21 @@ export default function Searchbar() {
         }else{
             setFilteredData(data.filter(a=>a.mainName===id))
         }
-        
     };
+
     const handleChange = (event) => {
         setSearchValue(event.target.value);
         const searchS = event.target.value;
-    
         const newArray = [];
-        const secondArray =[];
         
-    
         for (let i = 0; i < data.length - 1; i++)
         {
           let sub1 = data[i];
           if (sub1.mainName.toLowerCase().includes(searchS.toLowerCase()))
           {
-            // console.log('mainName', data[i].mainName);
-            // newArray.push(sub1)
+            newArray.push({
+                mainName: sub1.mainName.toLowerCase(),  
+              });
            
           }
       
@@ -40,9 +38,9 @@ export default function Searchbar() {
       
             if (sub2.subName.toLowerCase().includes(searchS.toLowerCase()))
             {
-            //   console.log('subName', sub2.subName);
-            //   newArray.push(sub2)
-            //   setSearchResult(newArray);
+            newArray.push({
+                subName: sub2.subName,
+              });
             }
       
             for (let b = 0; b < sub2.subDetails.length - 1; b++)
@@ -56,7 +54,6 @@ export default function Searchbar() {
                     item: sub2.subDetails[b].item,
                     itemNum: sub2.subDetails[b].itemNum,
                     subName: sub2.subName,
-                    // mainName: sub1.mainName.toLowerCase(),  
                   });
                   setSearchResult(newArray);
                
@@ -65,26 +62,75 @@ export default function Searchbar() {
           }
         }
         
-        // setIsSearch(false);
-        if(event.target.value.length<=0)return setIsSearch(false);
+        if(event.target.value.length<=0)return setIsSearch(false);        
+      };
+
+    const handleSearch=()=>{
+        const newArray = [];
+        if(searchValue===undefined){ setFilteredData(data); setIsSearch(true); }
+        else{
+        for (let i = 0; i < data.length - 1; i++)
+        {
+          let sub1 = data[i];
+          if (sub1.mainName.toLowerCase().includes(searchValue.toLowerCase()))
+          {
+            newArray.push({
+                mainName: sub1.mainName.toLowerCase(),  
+              });
+           
+          }
+      
+          for (let a = 0; a < sub1.sub.length - 1; a++)
+          {
+            let sub2 = sub1.sub[a];
+      
+            if (sub2.subName.toLowerCase().includes(searchValue.toLowerCase()))
+            {
+            newArray.push({
+                subName: sub2.subName,
+              });
+            }
+      
+            for (let b = 0; b < sub2.subDetails.length - 1; b++)
+            {
+              let last = sub2.subDetails[b].item.toLowerCase();
+              
+              if (last.includes(searchValue.toLowerCase()))
+              {
+                setIsSearch(true);
+                newArray.push({
+                    item: sub2.subDetails[b].item,
+                    itemNum: sub2.subDetails[b].itemNum,
+                    subName: sub2.subName,
+                  });
+                  setSearchResult(newArray);
+               
+              }
+            }
+          }
         console.log("newArray",newArray)
         
       };
-    const handleSearch=()=>{
         
-        const filteredItems = data.filter(item =>{
-            return item.mainName.toLowerCase().includes(searchValue.toLowerCase());
-          });
-        setFilteredData(filteredItems)
+        setFilteredData(newArray)
+    }
     };
-    // console.log("filtered data",filteredData)
+
     return (
         <>
-            <div className="search-bar-main">
-            {/* <i className="fa fa-search" onClick={handleSearch}></i> */}
+            <form className="search-bar-main">
+            <i 
+            className="fa fa-search" 
+            type="button" 
+            onClick={handleSearch}
+            style={{cursor: 'pointer'}}></i>
                 
-                <input type="text" id="search" name="search" placeholder="Search" onChange={handleChange}/>    
-            </div>
+                <input 
+                type="text"
+                id="search" 
+                name="search" 
+                placeholder="Search" onChange={handleChange}/>    
+            </form>
 
             <div className="select-info-main">
                 <h3 className="search-info-txt" onClick={()=>handleClick("ALL")}>
